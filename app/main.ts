4ff3858476a396b3aa0a9dd9be6b41f71cfefa8a
breezy-anj <windbornanjneya@gmail.com>
2026-07-06 01:48:19 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,12 @@
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// TODO: Uncomment the code below to pass the first stage
 const server = net.createServer((socket) => {
+  socket.write("HTTP/1.1 200 OK\r\n\r\n"
   socket.on("close", () => {
+    console.log("Connection Closed");
+
     socket.end();
   });
 });
