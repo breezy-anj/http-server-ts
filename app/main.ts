dc63da0da44f67acb5a540af7908716f9bec8b32
breezy-anj <windbornanjneya@gmail.com>
2026-07-06 01:48:30 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -3,7 +3,7 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n"
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
   socket.on("close", () => {
     console.log("Connection Closed");
 
