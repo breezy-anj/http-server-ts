4826cbc542f3a14985c833b8e8c5cffc074489bc
breezy-anj <windbornanjneya@gmail.com>
2026-07-06 02:11:00 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -3,7 +3,17 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (data) => {
+    const request = data.toString();
+    const path = request.split(" ")[1];
+
+    const response =
+      path === "/"
+        ? "HTTP/1.1 200 OK\r\n\r\n"
+        : "HTTP/1.1 404 Not Found\r\n\r\n";
+
+    socket.write(response);
+  });
   socket.on("close", () => {
     console.log("Connection Closed");
 
