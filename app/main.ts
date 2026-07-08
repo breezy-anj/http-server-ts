4fde0a9d7a70f92946bc64b820550a6dca89e05a
breezy-anj <windbornanjneya@gmail.com>
2026-07-08 19:33:49 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -2,19 +2,18 @@ import * as net from "net";
 
 console.log("Logs from your program will appear here!");
 
-const server = net.createServer((socket) => {
-  socket.on("data", (data) => {
+const server = net.createServer((socket: net.Socket) => {
+  socket.on("data", (data: Buffer) => {
     const request = data.toString();
     const path = request.split(" ")[1];
 
     const str = request.split(" ")[1];
-    const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n${str}`;
+    const strLen = str.length;
+    const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${strLen}\r\n\r\n${str}`;
     socket.write(response);
   });
   socket.on("close", () => {
     console.log("Connection Closed");
-
-    socket.end();
   });
 });
 
