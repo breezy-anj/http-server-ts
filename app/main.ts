1e26db8b3e1fb4b0e9190fc50b260df364166bc7
breezy-anj <windbornanjneya@gmail.com>
2026-07-08 19:58:51 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -7,11 +7,19 @@ const server = net.createServer((socket: net.Socket) => {
     const request = data.toString();
     const path = request.split(" ")[1];
 
-    const str = path.split("/")[2];
-    const strLen = str.length;
-    const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${strLen}\r\n\r\n${str}`;
-    socket.write(response);
-    socket.end();
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      socket.end();
+    } else if (path.startsWith("/echo/")) {
+      const str = path.split("/")[2];
+      const strLen = str.length;
+      const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${strLen}\r\n\r\n${str}`;
+      socket.write(response);
+      socket.end();
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      socket.end();
+    }
   });
   socket.on("close", () => {
     console.log("Connection Closed");
